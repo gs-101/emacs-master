4fffc7b81b6f9ab47534d88019230b74f147b41c
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2025-12-08 12:14:18 -0300

    fix: disable updates for now
    I think there's something wrong with package-vc-tests. I don't have time to
    investigate right now, so I'll just disable updates for now and revert the
    channel to a usable state.

--- emacs-master.scm
+++ emacs-master.scm
@@ -33,14 +33,14 @@
   #:use-module (guix git-download)
   #:use-module (guix download))
 
-(define emacs-master-commit "24297f6d9a2fb3bf464f25bdf80c85b13aa04a36")
+(define emacs-master-commit "11b68c6223d154b4c85e8feec63581dcf7c76a3e")
 (define emacs-master-igc-commit "f782dd29fb31c2f42429be1e9db4d95b85beb39f")
 
 ;; Returns the first seven characters of a commit.
 (define (shorthand-commit commit)
   (string-drop-right commit 33))
 
-(define emacs-master-hash "02lppgs6x3i5706288dy96rvw8qyxdy15kl4jk1ks4c0mkhz3spr")
+(define emacs-master-hash "13an7b9gkcd2b25nm66xcqvpps54fbhwi16315gd1n4gfa4b16pb")
 (define emacs-master-igc-hash "1q5kj8j9apy2lgpgfpvw3sda9bgal0z8z5gbqflqlj9764d6g74w")
 
 (define patches-path "patches/")
